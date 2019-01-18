b43fc34b5ddc39b918c0ed78c7d1e08b1458e711
Ihor Omelchenko <megapixel23@gmail.com>
2019-01-18 19:16:59 +0200

    Code prettifying.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const messages = require('./i18n/messages.ru')
 const VoiceMessageHandler = require('./handlers/VoiceMessageHandler')
 const IncomeMessageHandler = require('./handlers/IncomeMessageHandler')
 const { telegramToken, s3: s3Config } = require('./config/config')
-const { version } = require('./package')
+const { version } = require('./package.json')
 
 const incomeMessageHandler = new IncomeMessageHandler({ logger })
 const voiceMessageHandler = new VoiceMessageHandler({ telegramToken, logger, s3Config })
@@ -13,10 +13,8 @@ const voiceMessageHandler = new VoiceMessageHandler({ telegramToken, logger, s3C
 const bot = new Telegraf(telegramToken)
 
 bot.use((ctx, next) => incomeMessageHandler.process(ctx, next))
-bot.start(ctx => {
-  ctx.reply(messages.hello)
-})
-bot.help(ctx => ctx.reply(messages.sendMeASticker))
+bot.start(({ reply }) => reply(messages.hello))
+bot.help(({ reply }) => reply(messages.sendMeASticker))
 bot.on('sticker', ctx => ctx.reply(messages.stickerSent))
 bot.on('voice', ctx => voiceMessageHandler.process(ctx))
 bot.command('version', ({ reply }) => reply(version))
